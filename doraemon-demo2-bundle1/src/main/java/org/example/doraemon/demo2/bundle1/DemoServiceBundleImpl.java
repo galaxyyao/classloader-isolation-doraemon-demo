9eea1ece74973e72ada527491087a5ec5903c068
姚皓 <yaohao.yh@alibaba-inc.com>
2020-09-04 11:28:54 +0800

    NoSuchBeanDefinitionException的问题定位到了是devtools的问题，暂时先注释掉

--- doraemon-demo2-bundle1/src/main/java/org/example/doraemon/demo2/bundle1/DemoServiceBundleImpl.java
+++ doraemon-demo2-bundle1/src/main/java/org/example/doraemon/demo2/bundle1/DemoServiceBundleImpl.java
@@ -3,12 +3,19 @@ package org.example.doraemon.demo2.bundle1;
 import org.example.doraemon.demo2.facade.Demo2Facade;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @Author: yaohao.yh
  * @Date: 2020/9/4 10:03
  */
 @Component
 public class DemoServiceBundleImpl implements Demo2Facade {
+    @PostConstruct
+    public void init() {
+        System.out.println("DemoServiceBundleImpl initialized");
+    }
+
     @Override
     public void doSomething() {
         System.out.println("do something in bundle1");
