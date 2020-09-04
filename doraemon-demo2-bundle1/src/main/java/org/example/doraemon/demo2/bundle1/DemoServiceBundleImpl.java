77c640b3f2bd247f2283097d26866a605604321e
姚皓 <yaohao.yh@alibaba-inc.com>
2020-09-04 11:03:16 +0800

    补上bundle1的@Component

--- doraemon-demo2-bundle1/src/main/java/org/example/doraemon/demo2/bundle1/DemoServiceBundleImpl.java
+++ doraemon-demo2-bundle1/src/main/java/org/example/doraemon/demo2/bundle1/DemoServiceBundleImpl.java
@@ -1,11 +1,13 @@
 package org.example.doraemon.demo2.bundle1;
 
 import org.example.doraemon.demo2.facade.Demo2Facade;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author: yaohao.yh
  * @Date: 2020/9/4 10:03
  */
+@Component
 public class DemoServiceBundleImpl implements Demo2Facade {
     @Override
     public void doSomething() {
